b3981f37ec3c091a99806e35f528480f15848325
Alireza <ar.sedghi@gmail.com>
2023-09-26 12:22:38 -0400

    test new publish

--- publish-package.mjs
+++ publish-package.mjs
@@ -1,4 +1,10 @@
 import { execa } from 'execa';
+import fs from 'fs/promises';
+import glob from 'glob';
+import path from 'path';
+
+const MAX_RETRIES = 3;
+const RETRY_DELAY = 5000; // 5 seconds
 
 async function run() {
   const { stdout: branchName } = await execa('git', [
@@ -7,25 +13,56 @@ async function run() {
     'HEAD',
   ]);
 
-  if (branchName === 'main') {
-    await execa('npx', [
-      'lerna',
-      'publish',
-      'from-package',
-      '--no-verify-access',
-      '--yes',
-    ]);
-  } else {
-    // publish beta versions
-    await execa('npx', [
-      'lerna',
-      'publish',
-      'from-package',
-      '--no-verify-access',
-      '--yes',
-      '--dist-tag',
-      'beta',
-    ]);
+  const lernaJson = JSON.parse(await fs.readFile('lerna.json', 'utf8'));
+
+  const packages = lernaJson.packages;
+
+  for (const packagePathPattern of packages) {
+    const matchingDirectories = glob.sync(packagePathPattern);
+
+    for (const packageDirectory of matchingDirectories) {
+      const packageJsonPath = path.join(packageDirectory, 'package.json');
+
+      try {
+        const packageJsonContent = JSON.parse(
+          await fs.readFile(packageJsonPath, 'utf8')
+        );
+
+        if (packageJsonContent.private) {
+          console.log(`Skipping private package at ${packageDirectory}`);
+          continue;
+        }
+
+        process.chdir(packageDirectory);
+
+        let retries = 0;
+        while (retries < MAX_RETRIES) {
+          try {
+            const publishArgs = ['publish'];
+
+            if (branchName === 'master') {
+              publishArgs.push('--tag', 'beta');
+            }
+
+            await execa('npm', publishArgs);
+            console.log(
+              `Successfully published package at ${packageDirectory}`
+            );
+            break;
+          } catch (error) {
+            retries++;
+            console.error(
+              `Failed to publish package at ${packageDirectory}, retrying... (${retries}/${MAX_RETRIES})`
+            );
+            await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
+          }
+        }
+      } catch (error) {
+        console.error(
+          `An error occurred while processing ${packageDirectory}: ${error}`
+        );
+      }
+    }
   }
 
   console.log('Finished');
